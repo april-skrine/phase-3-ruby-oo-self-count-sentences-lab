39d6fb561465995d673e1e62e46717271fec900f
April Skrine <april.skrine@gmail.com>
2022-02-24 09:24:33 -0600

    commit tree 9c30c31dc8f7bc8d86ea5a96a9d3825a9e50d40d

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") 
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    counter = 0
+    self.split(/([\.\?\!])/).each do |split|
+      if split.include?(/\w/)
+        puts split
+      end
+    puts split
+    end
+    counter
   end
+
 end
